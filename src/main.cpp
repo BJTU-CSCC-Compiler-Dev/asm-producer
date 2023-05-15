46988ab71eff82436598bb18e1c7fc912536fb3e
ChrisLee <chirsleecn@outlook.com>
2023-05-16 01:43:38 +0800

    Test : Test gh action auto format

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,7 @@
 
 
 int main(int argc, char ** argv) {
-	config::init(argc, argv);
-	std::cout << config::get() << std::endl;
+	config::init(argc, argv); std::cout << config::get() << std::endl;
 
 	auto sysySrc = std::ifstream();
 	sysySrc.open(config::get().sysySrcFilePath);
